bb3dc130bc53a51f314d0fffd10e5e274800543b
Aditya Vishwakarma <72146140+AdityaVishwakarmaIN@users.noreply.github.com>
2024-10-14 01:57:21 +0530

    Update VLE.h

--- multi-component-distillation-column/VLE.h
+++ multi-component-distillation-column/VLE.h
@@ -7,8 +7,6 @@
 class VLE {
 public:
     std::vector<double> calculateKValues(const std::vector<Component>& components, double temperature, double pressure);
-
-    // placeholder for more VLE models
 };
 
-#endif // VLE_H
+#endif
